2adf181d7047d11ac10959a846d2b84f2d850b5e
feliperomano0 <113329949+feliperomano0@users.noreply.github.com>
2022-09-12 13:49:27 -0300

    Create pb_quiz.sql

---
+++ pb_quiz.sql
@@ -0,0 +1,12 @@
+REATE TABLE questoes(questao VARCHAR(255) NOT NULL,alternativa VARCHAR(255) NOT NULL,ativa VARCHAR(255) NOT NULL, PRIMARY KEY(questao));
+
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("O Urugia foi o primeiro pais a ganhar a copa?",true,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("A copa do mundo de 2022 sera no brasil",false,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("A copa do mundo de 2018 ocorreu no brasil",false,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("O brasil foi campeao da copa de 1994",true,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("O pelé é um jogador brasileiro?",true,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("O chile foi campeao da copa do mundo de 2010?",false,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("O brasil foi campeao da copa do mundo de 2022?",true,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("A copa do mundo de 1998 foi na Itália?",false,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("A Argentina foi campeã da copa do mundo em 1978?",true,false);
+INSERT INTO questoes (questao,alternativa,ativa) VALUES ("A Zâmbia ja participou da copa do mundo?",false,false);
